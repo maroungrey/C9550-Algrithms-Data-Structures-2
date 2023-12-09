9c6d80824aa2fb6d9936813dbffaa5248edc0829
maroungrey <maroun.barqawi@gmail.com>
2023-12-09 08:35:09 -0800

    Fucked with this all night, fixed the bugs, now it displays the route correctly

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from package import Package
 from truck import Truck
 from csvReader import CSVReader
 from hashTable import HashMap
+import logging
 
 # Load packages from the Package file
 package_file_path = 'CSV/Package.csv'
@@ -33,7 +34,7 @@ for package in packages:
     package_hash_map.insert(package.package_id, package)
 
 # Manually determine which packages go into which truck
-assigned_package_ids = [14, 23, 21]
+assigned_package_ids = [15, 22, 24]
 for package_id in assigned_package_ids:
     package = package_hash_map.lookup(package_id)
     if package:
@@ -55,28 +56,28 @@ print("Truck 2 loaded packages:")
 for loaded_package in truck2.packages:
     print(f"Package ID: {loaded_package.package_id}, Address: {loaded_package.address}")
 
-print("\nTruck 3 loaded packages:")
+print("Truck 3 loaded packages:")
 for loaded_package in truck3.packages:
     print(f"Package ID: {loaded_package.package_id}, Address: {loaded_package.address}")
 
 
 # Extract the hub address from the first row of distance_data
-HUB_ADDRESS = distance_data[1]['Address']
+HUB_ADDRESS = distance_data[0]['Address']
+
 
 def extract_route_addresses(truck, distance_data):
     # Extract unique addresses from loaded packages
     addresses = set(package.address for package in truck.packages)
     
-    # Extract the hub address from the first row of distance_data
-    hub_address = distance_data[1]['Address']  # Assuming the 'Address' column contains hub addresses
-    
-    # Add the hub address as the starting point
-    addresses.add(hub_address)
+    # Add the hub address as the starting point if it's not already in the set
+    if HUB_ADDRESS not in addresses:
+        addresses.add(HUB_ADDRESS)
     
     # Convert set to a list and sort it for the route
-    route_addresses = sorted(list(addresses), key=lambda x: x if x != hub_address else '')
+    route_addresses = sorted(list(addresses), key=lambda x: x != HUB_ADDRESS)
     
     return route_addresses
+
 
 
 def get_distance_from_csv(start_address, end_address, distance_data):
@@ -111,7 +112,8 @@ def get_distance_to_or_from_hub(address, distance_data, to_hub=True):
 
         # Get the distance to or from the hub and convert it to a float
         distance_key = '4001 South 700 East'  # Adjust this based on the actual header for hub distances
-        distance = float(row[distance_key]) if to_hub else float(row[distance_key])
+        hub_distance_key = '4001 South 700 East'  # Adjust this based on the actual header for hub distances
+        distance = float(row[hub_distance_key]) if to_hub else float(row[distance_key])
 
         return distance
     except StopIteration:
@@ -123,22 +125,30 @@ def get_distance_to_or_from_hub(address, distance_data, to_hub=True):
     except ValueError:
         print(f"Error: Distance value is not a valid number for {address}")
         return None
+
     
+
+
+
+
 def send_truck_on_route(truck, distance_data):
     # Get the route addresses dynamically
     route_addresses = extract_route_addresses(truck, distance_data)
 
-    # Print distance from the hub to the first location
-    hub_to_first_location_distance = get_distance_to_or_from_hub(route_addresses[0], distance_data)
-    print(f"Distance from Hub to {route_addresses[0]}: {hub_to_first_location_distance} miles")
-
     # Initialize current time based on the departure time
     current_time = truck.time_left_hub
 
-    # Iterate through the route
-    for i in range(len(route_addresses) - 1):
-        start_address = route_addresses[i]
-        end_address = route_addresses[i + 1]
+    # Iterate through the route starting from the second address
+    for i in range(1, len(route_addresses)):
+        start_address = route_addresses[i - 1]
+        end_address = route_addresses[i]
+
+        # Get the package ID for the current address
+        package_id = None
+        for package in truck.packages:
+            if package.address == end_address:
+                package_id = package.package_id  # Replace 'package_id' with your actual attribute name
+                break
 
         # Get the distance from the CSV data
         distance = get_distance_from_csv(start_address, end_address, distance_data)
@@ -150,7 +160,7 @@ def send_truck_on_route(truck, distance_data):
         delivery_time = current_time + timedelta(hours=travel_time_hours)
 
         # Print the distance and estimated delivery time
-        print(f"Distance from {start_address} to {end_address}: {distance} miles, Estimated Delivery Time: {delivery_time.strftime('%I:%M %p')}")
+        print(f"Package ID: {package_id}, Distance from {start_address} to {end_address}: {distance} miles, Estimated Delivery Time: {delivery_time.strftime('%I:%M %p')}")
 
         # Update the current location and time of the truck
         truck.update_current_location(end_address)
@@ -160,8 +170,8 @@ def send_truck_on_route(truck, distance_data):
     last_location_to_hub_distance = get_distance_to_or_from_hub(route_addresses[-1], distance_data, to_hub=False)
     last_location_to_hub_travel_time = last_location_to_hub_distance / 18
     last_location_to_hub_delivery_time = current_time + timedelta(hours=last_location_to_hub_travel_time)
-    print(f"Distance from {route_addresses[-1]} to Hub: {last_location_to_hub_distance} miles, Estimated Delivery Time: {last_location_to_hub_delivery_time.strftime('%I:%M %p')}")
+    print(f"Distance from {route_addresses[-1]} to WGU {HUB_ADDRESS}: {last_location_to_hub_distance} miles, Arrival Time: {last_location_to_hub_delivery_time.strftime('%I:%M %p')}")
 
 # Example of sending truck2 on its route
-print("Distances:")
+print("\nDistance & Arrival Time:")
 send_truck_on_route(truck2, distance_data)
